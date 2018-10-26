d0734ae06aa9fdc0e431313ea21c5b696c9191df
Julian Rodriguez <felorodri@gmail.com>
2018-10-26 17:55:00 -0500

    firebase-config file info removed

--- public/js/config/firebase-config.js
+++ public/js/config/firebase-config.js
@@ -1,10 +1,10 @@
 var projectConfig = {
-    apiKey: "AIzaSyDvEwv-mudgDHMIF4407J84Jd9cqk0kwU4",
-    authDomain: "sigetec-dev.firebaseapp.com",
-    databaseURL: "https://sigetec-dev.firebaseio.com",
-    projectId: "sigetec-dev",
-    storageBucket: "sigetec-dev.appspot.com",
-    messagingSenderId: "396670079668"
+    apiKey: "YOUR_API_KEY_HERE",
+    authDomain: "YOUR_AUTH_DOMAIN_HERE",
+    databaseURL: "YOUR_DATABASE_URL_HERE",
+    projectId: "YOUR_PROJECT_ID_HERE",
+    storageBucket: "YOUR_STORAGE_BUCKET_HERE",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID_HERE"
 };
 
 firebase.initializeApp(projectConfig);
